f0aa5eb8fc3094717f948e478a6269eaa2e2564a
witaminsiz <witam@DESKTOP-I9L55Q5>
2020-07-12 14:16:02 +0300

    employee class has been written to use linq examples. This custom class will be sorted using "Linq" and it will be used to do some filter for this objects.

---
+++ LinqQueryListAndGenericType/LinqQueryListAndGenericType/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace LinqQueryListAndGenericType
+{
+    class Employee
+    {
+        public string firstName { get; }
+        public string lastName { get; }
+        private decimal monthlySalary;
+        public Employee(string firstName, string lastName, decimal monthlySalary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.monthlySalary = monthlySalary;
+        }
+        public decimal MontlySalary
+        {
+            get { return monthlySalary; }
+            set { if (MontlySalary >= 0M) monthlySalary = MontlySalary; }
+        }
+        //public override string ToString()
+        //{
+        //    return $"{firstName,-10}{lastName,-10}{monthlySalary,10:C}";
+        //}
+        public override string ToString() => $"{firstName,-10}{lastName,-10}{monthlySalary,10:C}";
+    }
+    
+}
